be71d6a4e946b0a4477214903ef8b89ca4dd6b99
Lawful <87284804+Lawful831@users.noreply.github.com>
2021-07-26 11:00:54 -0500

    Added a Keybinding System
    I added the Keybinding System

--- RhythmGameJam/Assets/Scripts/ButtonHandler.cs
+++ RhythmGameJam/Assets/Scripts/ButtonHandler.cs
@@ -1,23 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using TMPro;
 using UnityEngine;
 
 public class ButtonHandler : MonoBehaviour
 {
+    //publics
     public string play;
     public string credits;
     public string mainmenu;
+    public GameObject settings;
+    public GameObject MainM;
+    public GameObject RemapMessage;
+    public TextMeshProUGUI remaptxt;
 
+    //privs
+    [SerializeField]
+    bool remapping;
     public void Play() {
         Debug.Log("<color=yellow>"+play+"</color>");
         //SceneManager.LoadScene(play);
     }
 
+    public void Update()
+    {
+        if (Input.anyKeyDown && remapping)
+        {
+            Debug.Log("You pressed " + Input.inputString);
+        }
+    }
     public void Credit()
     {
         Debug.Log("<color=yellow>" + credits + "</color>");
         //SceneManager.LoadScene(credits);
+    }
+
+    public void Settings()
+    {
+        MainM.SetActive(false);
+        Debug.Log("Settings");
+        settings.SetActive(true);
+
+    }
+
+    public void Back()
+    {
+        settings.SetActive(false);
+        Debug.Log("Menu");
+        MainM.SetActive(true);
     }
 
     public void Quit()
@@ -26,8 +57,4 @@ public class ButtonHandler : MonoBehaviour
         Application.Quit();
     }
 
-    public void MainMenu()
-    {
-        SceneManager.LoadScene(mainmenu);
-    }
 }
