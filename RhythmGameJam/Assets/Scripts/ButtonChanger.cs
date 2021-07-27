58dbce5acb935a650413acb80e2cf4b27fe93877
Lawful <87284804+Lawful831@users.noreply.github.com>
2021-07-27 11:28:15 -0500

    Added feautures to the SushiSong

--- RhythmGameJam/Assets/Scripts/ButtonChanger.cs
+++ RhythmGameJam/Assets/Scripts/ButtonChanger.cs
@@ -4,25 +4,36 @@ using UnityEngine;
 
 public class ButtonChanger : MonoBehaviour
 {
-    public KeyCode pressedKey;
+    public bool isUp;
     public Sprite normalButton;
     public Sprite pressedButton;
     SpriteRenderer sr;
+    private KeyBinding kb;
 
     // Start is called before the first frame update
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        kb = GameObject.Find("KeyBindManager").GetComponent<KeyBinding>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(pressedKey))
+        if(Input.GetKeyDown(kb.keys["UpKey"]) && isUp)
         {
             sr.sprite = pressedButton;
         }
-        if(Input.GetKeyUp(pressedKey))
+        if(Input.GetKeyUp(kb.keys["UpKey"]) && isUp)
+        {
+            sr.sprite = normalButton;
+        }
+
+        if (Input.GetKeyDown(kb.keys["DownKey"]) && !isUp)
+        {
+            sr.sprite = pressedButton;
+        }
+        if (Input.GetKeyUp(kb.keys["DownKey"]) && !isUp)
         {
             sr.sprite = normalButton;
         }
