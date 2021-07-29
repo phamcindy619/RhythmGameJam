f638528ab655f0e5bc8e2d5ce87395a595af7fed
Cindy Pham <phamcindy619@utexas.edu>
2021-07-29 16:12:49 -0500

    NoteSpawner works with new version of NoteCharter

--- RhythmGameJam/Assets/Scripts/NoteSpawner.cs
+++ RhythmGameJam/Assets/Scripts/NoteSpawner.cs
@@ -9,52 +9,42 @@ public class NoteSpawner : MonoBehaviour
     private NoteScroller ns;
     public Transform UpLane;
     public Transform DownLane;
-    public GameObject NoteUplane;
+    public GameObject NoteUpLane;
     public GameObject NoteDownLane;
-    public GameObject notescroller;
-    public string Pathtocsv = "Assets/Csv/song.csv";
-    //public Dictionary<float, System.Tuple<string,string>> NoteTimes = new Dictionary<float, System.Tuple<string,string>>();
+    public string pathToCsv = "Assets/Csv/Chicken_Alfredo02.csv";
 
     // Note spawner based on beat - Cindy
     public SongManager sm;
-    float[] notes = {1f, 2f, 3f, 4f, 7f};
+    public struct Note {
+        public float beat;
+        public bool upLane;
+    };
+    List<Note> notes;
     int nextIndex = 0;
-
 
     void Start()
     {
         sm = GameObject.Find("SongManager").GetComponent<SongManager>();
+        notes = new List<Note>();
+        ReadCsvFile(pathToCsv);
     }
 
     void Update()
     {
         if (ns.hasStarted) {
-            if (nextIndex < notes.Length && notes[nextIndex] < sm.songPositionInBeats) {
-                //Generates a random number between 0 and 1-Lawful
-                float random = Random.Range(0f, 1f);
-                Debug.Log(random);
-                //If the random number is greater than .5 create a UpNote else create a DownNote-Lawful
-                if(random > .5)
+            if (nextIndex < notes.Count && notes[nextIndex].beat < sm.songPositionInBeats) {
+                if(notes[nextIndex].upLane)
                 {
-                    GameObject n = Instantiate(NoteUplane, UpLane.transform.position, Quaternion.identity);
-                    n.transform.parent = notescroller.transform;
+                    GameObject n = Instantiate(NoteUpLane, UpLane.transform.position, Quaternion.identity);
+                    n.transform.parent = ns.gameObject.transform;
                 }
                 else
                 {
                     GameObject n = Instantiate(NoteDownLane, DownLane.transform.position, Quaternion.identity);
-                    n.transform.parent = notescroller.transform;
+                    n.transform.parent = ns.gameObject.transform;
                 }
-
                 nextIndex++;
             }
-        }
-    }
-
-    private void FixedUpdate()
-    {
-        if (ns.hasStarted)
-        {
-            //ReadCsvFile(Pathtocsv);
         }
     }
 
@@ -70,46 +60,14 @@ public class NoteSpawner : MonoBehaviour
                 endoffile = true;
                 break;
             }
-            var data_values = data_string.Split(',');
-            //NoteTimes.Add(data_values[0]);
-            if (Time.time.ToString("0.00") == data_values[0])
-            {
-                if(data_values[1] == "normal")
-                {
-                    if(data_values[2] == "up")
-                    {
-                        Debug.Log("Normal Note! Up Lane");
-                        CreateNote(true);
+            string[] data_values = data_string.Split(',');
 
-                    }
-                    else if (data_values[2] == "down")
-                    {
-                        Debug.Log("Normal Note! Down Lane");
-                        CreateNote(false);
-                    }
-                }
-                else if(data_values[1] == "special")
-                {
-                    Debug.Log("Special Note!");
-                }
-            }
+            Note newNote = new Note {
+                beat = float.Parse(data_values[0]),
+                upLane = data_values[1] == "up"? true : false
+            };
+
+            notes.Add(newNote);
         }
     }
-
-    void CreateNote(bool up)
-    {
-        if (up)
-        {
-            GameObject n = Instantiate(NoteUplane, UpLane.transform.position, Quaternion.identity);
-            n.transform.parent = notescroller.transform;
-        }
-        else
-        {
-
-            GameObject n = Instantiate(NoteDownLane, DownLane.transform.position, Quaternion.identity);
-            n.transform.parent = notescroller.transform;
-        }
-    }
-
-
 }
