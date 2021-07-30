4b5cb7c0a1ef7a743715433bb53562acf6564739
Cindy Pham <phamcindy619@utexas.edu>
2021-07-30 13:49:40 -0500

    Fixed rating system

--- RhythmGameJam/Assets/Scripts/Rating.cs
+++ RhythmGameJam/Assets/Scripts/Rating.cs
@@ -7,11 +7,11 @@ public class Rating : MonoBehaviour
 {
     //Getting the total score(with the combo affecting included), the score of the player, and the Sprites of the Grades
     public GameObject ActiveUI;
-    public Image rateimage;
+    public Image rateImage;
     public GameObject RateMenu;
     public int totalScore;
     public int Score;
-    private Sprite S, A, B, C, D, F;
+    public Sprite S, A, B, C, D, F;
 
     private void Start()
     {
@@ -41,34 +41,30 @@ public class Rating : MonoBehaviour
     {
         //Get the sixth of the totalscore
         int sixth = totalScore / 6;
-        //If the score is a 1/6 of the total score, is a F grade
-        if(score <= sixth)
-        {
-            rateimage.sprite = F;
-            Debug.Log("F score!");
-        }else if(score <= sixth * 2)//If the score is a 2/6 of the total score, is a D grade
-        {
-            rateimage.sprite = D;
-            Debug.Log("D score!");
+
+        if (score > sixth * 6) {
+            rateImage.sprite = S;
+            Debug.Log("S score!");
         }
-        else if (score <= sixth * 3)//If the score is a 3/6 of the total score, is a C grade
-        {
-            rateimage.sprite = C;
-            Debug.Log("C score!");
+        else if (score > sixth * 5) {
+            rateImage.sprite = A;
+            Debug.Log("A score!");
         }
-        else if (score <= sixth * 4)//If the score is a 4/6 of the total score, is a B grade
-        {
-            rateimage.sprite = B;
+        else if (score > sixth * 4) {
+            rateImage.sprite = B;
             Debug.Log("B score!");
         }
-        else if (score <= sixth * 5)//If the score is a 5/6 of the total score, is a A grade
-        {
-            rateimage.sprite = A;
-            Debug.Log("A score!");
-        }else if(score <= sixth * 6)//If the score is a 6/6 of the total score, is a S grade
-        {
-            rateimage.sprite = S;
-            Debug.Log("S score!");
+        else if (score > sixth * 3) {
+            rateImage.sprite = C;
+            Debug.Log("C score!");
+        }
+        else if (score > sixth * 2) {
+            rateImage.sprite = D;
+            Debug.Log("D score!");
+        }
+        else {
+            rateImage.sprite = F;
+            Debug.Log("F score!");
         }
     }
 }
