1f2eba27f9ffa0f8ec300443f68cc854d44ae4b1
Lawful <87284804+Lawful831@users.noreply.github.com>
2021-07-31 09:20:27 -0500

    Fixed Keybinding Bug

--- RhythmGameJam/Assets/Scripts/Managers/KeyBinding.cs
+++ RhythmGameJam/Assets/Scripts/Managers/KeyBinding.cs
@@ -9,16 +9,17 @@ public class KeyBinding : MonoBehaviour
     public Dictionary<string, KeyCode> keys = new Dictionary<string, KeyCode>();
     public Text UpKey;
     public Text DownKey;
-
+    [SerializeField]
     private GameObject currentKey;
-
+    private Color32 normal = new Color32(255, 255, 255, 255);
+    private Color32 pressed = new Color32(255, 0, 93, 240);
     public static KeyBinding instance = null;
 
     // Start is called before the first frame update
     void Start()
     {
-        keys.Add("UpKey",(KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("UpKey", "F")));
-        keys.Add("DownKey", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("DownKey", "J")));
+        keys.Add("UpKey",(KeyCode) System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("UpKey", "Z")));
+        keys.Add("DownKey", (KeyCode)System.Enum.Parse(typeof(KeyCode), PlayerPrefs.GetString("DownKey", "X")));
 
         if(UpKey != null)
         {
@@ -55,6 +56,7 @@ public class KeyBinding : MonoBehaviour
             {
                 keys[currentKey.name] = e.keyCode;
                 currentKey.transform.GetChild(0).GetComponent<Text>().text = e.keyCode.ToString();
+                currentKey.GetComponent<Image>().color = normal;
                 currentKey = null;
             }
         }
@@ -62,7 +64,12 @@ public class KeyBinding : MonoBehaviour
 
     public void ChangeKey(GameObject clicked)
     {
+        if (currentKey != null)
+        {
+            currentKey.GetComponent<Image>().color = normal;
+        }
         currentKey = clicked;
+        currentKey.GetComponent<Image>().color = pressed;
     }
 
     public void SaveKeys()
@@ -70,6 +77,7 @@ public class KeyBinding : MonoBehaviour
         foreach (var key in keys)
         {
             PlayerPrefs.SetString(key.Key, key.Value.ToString());
+            Debug.Log("UpKeys: " + PlayerPrefs.GetString("UpKey") + "Down Keys:" + PlayerPrefs.GetString("DownKey"));
         }
         PlayerPrefs.Save();
     }
